fc69e730b16e3cdccfabfcdfc2674b6d38fb46b4
Biza_13 <serrano_0_1@hotmail.com>
2025-04-18 10:54:03 +0200

    haciendo pruebas con s3 como backend en lugar de subirlo en actions

--- archivos-tf/s3.tf
+++ archivos-tf/s3.tf
@@ -7,3 +7,30 @@ resource "aws_s3_bucket" "s3"{
     Enviroment = "Dev"
   }
 }
+
+#dynamoDB
+resource "aws_dynamodb_table" "tabla_dynamodb"{
+  name = "tfstate-bloqueo"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"  #clave primaria
+
+  attribute {
+    name = "LockID"  #El attributo clave primaria
+    type = "S"  #Va a ser tipo string
+  }
+
+  tags = {
+    Enviroment = "Dev"
+    name = "tfstate-bloqueo"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = var.s3
+    key = "terraform.tfstate" # Ruta del archivo tfstate en el bucket
+    region = "us-east-1" # Región del bucket
+    dynamodb_table = "tfstate-bloqueo" # Nombre de la tabla DynamoDB
+    encrypt = true # Cifrar el estado
+  }
+}
