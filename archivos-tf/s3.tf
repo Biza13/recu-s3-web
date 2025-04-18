d4c7de435401b83330b8b3012f24d3cb714c7051
Biza_13 <serrano_0_1@hotmail.com>
2025-04-18 11:47:39 +0200

    no se admiten variables en el bloque de backend

--- archivos-tf/s3.tf
+++ archivos-tf/s3.tf
@@ -28,7 +28,7 @@ resource "aws_dynamodb_table" "tabla_dynamodb"{
 #backend usando el s3 que creo previamente en actions
 terraform {
   backend "s3" {
-    bucket         = var.s3
+    bucket         = "cubo-s3-begona"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tfstate-bloqueo"
